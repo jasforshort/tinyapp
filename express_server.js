ea94b50f6d5742f7f4be0b077007b3805ab82569
Jas Mann <jaforshort@hotmail.ca>
2022-07-04 18:07:23 +0000

    Implemented the user object within _header and express_server

--- express_server.js
+++ express_server.js
@@ -66,14 +66,14 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]]};
   res.render("urls_new", templateVars);
 });
 
@@ -86,7 +86,7 @@ app.post("/urls", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[req.cookies["user_id"]],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
@@ -121,7 +121,7 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req,res) => {
-  let templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render("urls_register", templateVars);
 });
 
