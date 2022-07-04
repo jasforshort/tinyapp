d4aa496dcba5bcd50a0084d1787b1211b3e1b0a5
Jas Mann <jaforshort@hotmail.ca>
2022-07-04 17:55:47 +0000

    Added a registration page

--- express_server.js
+++ express_server.js
@@ -87,6 +87,11 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req,res) => {
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_register", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
