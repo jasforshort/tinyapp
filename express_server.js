d12c2b3c215b9f287e1eca90f4fcaae8fd30eb2c
Jas Mann <jaforshort@hotmail.ca>
2022-07-10 19:22:30 +0000

    Removed cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 const bcrypt = require('bcryptjs');
 const cookieSession = require('cookie-session');
